dfe77c50b71a7ab2f5b4a72b11ccc85687ef62ab
beatylunch <464145564@qq.com>
2014-02-18 09:47:00 +0800

    函数
    函数

---
+++ function3/function3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void stealfood()
+{
+printf("一只老鼠偷吃东西，");
+}
+void discovery()
+{
+printf("猫发现了老鼠，");
+}
+void trace()
+{
+printf("猫追老鼠，");
+}
+void run()
+{
+printf("老鼠飞跑，");
+}
+void awake()
+{
+printf("惊醒了小狗，");
+}
+void yad()
+{
+printf("小狗吠。");
+}
+main()
+{
+stealfood();discovery();trace();run();awake();yad();
+
+getchar();
+}
