55ba7443ffa19d4350fc4fb3cb99f63510aaae17
beatylunch <464145564@qq.com>
2014-02-20 10:24:33 +0800

    函数

---
+++ function3/函数修改.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main()
+{
+	char stealfood(char x[]);
+	char a[]={"一只老鼠偷吃东西，"};
+	char c[]={""};
+printf("%s",stealfood(c));
+
+
+getchar();
+}
+
+char stealfood(char x[])
+{
+
+	char z[];
+strcpy(z[],x[]);
+    return(z);
+}
