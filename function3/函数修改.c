cdf284ffc75b2e9dac04a0e35188b3d6dfe7594f
beatylunch <464145564@qq.com>
2014-02-21 14:34:14 +0800

    小狗题做完
    函数修改完成

--- function3/函数修改.c
+++ function3/函数修改.c
@@ -3,9 +3,23 @@
 main()
 {
 	char stealfood(char x[]);
+	char discovery(char x[]);
+	char trace(char x[]);
+	char run(char x[]);
+	char awake(char x[]);
+	char yad(char x[]);
 	char a[]={"一只老鼠偷吃东西，"};
-	char c[]={""};
-printf("%s",stealfood(c));
+	char b[]={"猫发现了老鼠，"};
+	char c[]={"猫追老鼠，"};
+	char d[]={"老鼠飞跑，"};
+	char e[]={"惊醒了小狗，"};
+	char f[]={"小狗吠。"};
+	stealfood(a);
+	discovery(b);
+	trace(c);
+	run(d);
+	awake(e);
+	yad(f);
 
 
 getchar();
@@ -14,7 +28,38 @@ getchar();
 char stealfood(char x[])
 {
 
-	char z[];
-strcpy(z[],x[]);
-    return(z);
+printf("%s",x);
+
 }
+char discovery(char x[])
+{
+
+printf("%s",x);
+
+}
+char trace(char x[])
+{
+
+printf("%s",x);
+
+}
+char run(char x[])
+{
+
+printf("%s",x);
+
+}
+char yad(char x[])
+{
+
+printf("%s",x);
+
+}
+char awake(char x[])
+{
+
+printf("%s",x);
+
+}
+
+
