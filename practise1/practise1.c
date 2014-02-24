a1749505a8fd487b89b64ccb9360259849d7ffd8
beatylunch <464145564@qq.com>
2014-02-24 16:45:12 +0800

    函数调用修改完成

--- practise1/practise1.c
+++ practise1/practise1.c
@@ -8,10 +8,12 @@ return(z);
 main()
 {
 int a,b,c,d,e;
-a=1;b=0;c=2;d=3;e=2;
-c=file(d+e,c);
-b=file(c,b);
-a=file(b,a);
+	a=1;b=0;c=2;d=3;e=2;
+a=file(file(a,b),file(d+e,c));
+	b=0;c=2;d=3;e=2;
+b=file(file(c,d+e),b);
+	c=2;d=3;e=2;
+c=file(d+e,c);		
 printf("The file is:A=%d,B=%d,C=%d,D=%d,E=%d",a,b,c,d,e);
 
 getchar();
