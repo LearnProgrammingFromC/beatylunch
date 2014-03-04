b45e97a15f252ad4ad972f029d9cdf339982c540
beatylunch <464145564@qq.com>
2014-03-04 16:49:47 +0800

    更改

--- once again/once again.c
+++ once again/once again.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 main()
 {
-int i;
-float x;
-float y;
-float z=0;
-printf("please input number:");
-scanf("%f",&x);
+	int i;
+	float x;
+	float y;
+	float z=0;
+	printf("please input number:");
+	scanf("%f",&x);	//键入X的值
 if(x==1)
 {
-y=x;
+	y=x;   			//避免是X=0的情况
 }
-else
+	else
 for(i=2;i<=x;x--)	//控制循环的次数，
 {
-y=1/x+1/(x-1);
-x=x-1;
-z=z+y;
+	y=1/x+1/(x-1);
+	x=x-1;
+	z=z+y;
 }
-printf("%f",z);
+	printf("%f",z);  //用Z输出累加
 
 
-getchar();
+getchar()；				//停住cmd
 }
